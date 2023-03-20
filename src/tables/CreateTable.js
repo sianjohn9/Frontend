f10849155782f559374d517337122289a3e36c24
John Sian <115193516+sianjohn9@users.noreply.github.com>
2023-03-19 19:08:37 -0700

    Create CreateTable.js

---
+++ src/tables/CreateTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import FormTable from "../tables/FormTable";
+
+function CreateTable() {
+  return (
+    <main>
+      <h1>Create a New Table</h1>
+      <FormTable />
+    </main>
+  );
+}
+
+export default CreateTable;
